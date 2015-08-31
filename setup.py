b01b530c77b66847d48f91155bf2b4e7f121ad46
Albert White <albert.white@gmail.com>
2015-08-31 23:40:47 +0100

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyskyx',
+      version='0.1',
+      description='Module to interface with Software Bisques TheSkyX',
+      url='https://github.com/albertw/pyskyx',
+      author='Albert White',
+      author_email='albert.white@gmail.com',
+      license='',
+      py_modules=['skyx'],
+      zip_safe=False)
